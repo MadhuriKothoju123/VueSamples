07c772c3e83ae05b1e3d89fbc43e0ac17e1a03f0
MadhuriKothoju123 <kothojumadhuri2000@gmail.com>
2024-05-02 10:55:32 +0530

    initialized git repo

---
+++ vue_without_cli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  
+    
+        <h1>Vue - TypeScript - Webpack</h1>
+    </template>
+    
+    <script setup>
+ 
+    </script>
+    
+    <style scoped>
+    img{
+        display: block;
+        margin: 100px auto 10px;
+    }
+    h1{
+        width: 100%;
+        text-align: center;
+        margin-top: 10px;
+    }
+    </style>
---
+++ vue_without_cli/src/main.js
@@ -0,0 +1,9 @@
+import {createApp} from 'vue';
+
+import './static/global.css';
+import App from './App.vue'
+
+
+createApp(App ).mount('#app');
+
+//alert("Helo");
